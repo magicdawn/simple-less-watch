2d641abe4ad26730988127e466e6d2a13cc3a871
magicdawn <magicdawn@qq.com>
2015-04-28 14:15:55 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# simple-lessc-watch
+
+## Install
+```sh
+npm i simple-lessc-watch -g
+```
+
+## Usage
+```sh
+lessc-watch index.less index.css
+```
+
+## License
+the MIT License (magicdawn@qq.com)
---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env iojs
+
+var cp = require('child_process');
+var path = require('path');
+var fs = require('fs');
+
+/**
+ * lessc-watch abc.less [xxx-options]
+ */
+var args = process.argv.slice(2); // abc.less [xxx-options]
+var file = args[0];
+if (!file) {
+  console.log(`
+  usage :
+
+    less-watch abc.less [xxx-options]`);
+
+  process.exit(1);
+}
+
+file = path.resolve(file);
+var watcher = fs.watch(file, function(ev, filename) {
+  if (ev === 'change') {
+    console.log('>>>『●』file changed,recompiling ...');
+    compile();
+  }
+});
+compile(); // compile for 1st time
+
+
+/**
+ * indicate whether is compiling
+ */
+var isCompiling;
+
+/**
+ * do compile each time
+ */
+function compile() {
+  if (isCompiling) return;
+  isCompiling = true;
+
+  var exe = 'lessc';
+  if (process.platform === 'win32') {
+    exe += '.cmd';
+  }
+
+  var less = cp.spawn(exe, args, {
+    stdio: 'inherit'
+  });
+
+  less.on('exit', function(code) {
+    isCompiling = false;
+
+    if (code > 0) {
+      console.log('>>>『×』exit with code : %s , seems faild.', code);
+    } else {
+      console.log('>>>『√』less compiled.');
+    }
+  });
+}
